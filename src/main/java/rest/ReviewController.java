625dbf965c7a15ef5fdd9c18fc52efeabb3a1261
Arsim Ljoki <113034331+ArsimBoy420@users.noreply.github.com>
2024-11-08 12:33:58 +0100

    glemte den her

--- src/main/java/rest/ReviewController.java
+++ src/main/java/rest/ReviewController.java
@@ -1,11 +1,20 @@
 package rest;
 
-import facades.ReviewFacade;
-import dtos.ReviewDTO;
-import javax.ws.rs.*;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+
+import dtos.ReviewDTO;
+import facades.ReviewFacade;
 
 @Path("/review")
 public class ReviewController {
